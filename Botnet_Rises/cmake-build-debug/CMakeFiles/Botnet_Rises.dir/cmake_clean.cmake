394c77741f1ccc57b2665d59d497d758a333cc1e
krilliman <kristmann.ingi@gmail.com>
2019-10-06 10:57:17 +0000

    Skeleton for listening to servers and clients

---
+++ Botnet_Rises/cmake-build-debug/CMakeFiles/Botnet_Rises.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Botnet_Rises.dir/main.cpp.o"
+  "Botnet_Rises.pdb"
+  "Botnet_Rises"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Botnet_Rises.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Botnet_Rises/cmake-build-debug/CMakeFiles/Botnet_Rises.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Botnet_Rises.
+# This may be replaced when dependencies are built.
